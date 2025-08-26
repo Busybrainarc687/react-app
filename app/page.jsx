c6b43f599f74d9da19dbe99576807950fc8ff470
BIG DREAMz CODING <fam687arc@gmail.com>
2025-08-26 05:45:10 -0700

    changes

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+     <h1 className="bg-red-500 text-white p-5 text-2xl font-bold text-center">
+      THIS IS THE MAIN BRANCH
+     </h1>
+    </div>
+  );
+}
